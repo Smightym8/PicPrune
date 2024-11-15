894c4269a305d29de30e2c8086e1a233b50f2a72
Michael Spiegel <michaelspiegel@live.at>
2024-11-15 16:41:06 +0100

    Added file dialog to select directory
    Co-authored-by: 1r-1na <irij666@gmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
+
+def select_directory():
+    directory = filedialog.askdirectory()
+    print(directory)
 
 root = Tk()
+root.title('PicPrune')
+root.minsize(width=800, height=500)
 frm = ttk.Frame(root, padding=10)
-frm.grid()
 
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+frm.grid()
+ttk.Button(frm, text="Select Directory", command=select_directory).grid(column=0, row=0)
 
 root.mainloop()
