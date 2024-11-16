d0b04ea3715248093c9ee611a4de1d4e74cfc003
1r-1na <irij666@gmail.com>
2024-11-16 15:41:55 +0100

    Added logic to check for similar images using sentence-transformers
    Co-authored-by: Smightym8 <michael.spiegel@students.fhv.at>

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 import hashlib
+from sentence_transformers import SentenceTransformer, util
+from PIL import Image
 import os
 
 DUPLICATES_DIRECTORY_NAME = 'duplicates'
+SIMILARITIES_DIRECTORY_NAME = 'similarities'
 image_extensions = ('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff', '.webp')
 directory = None
-duplicatesDirectory = None
+duplicates_directory = None
+similarities_directory = None
 
 def select_directory():
     global directory
-    global duplicatesDirectory
+    global duplicates_directory
+    global similarities_directory
+    
     directory = filedialog.askdirectory()
     if directory:
         selected_dir_label.config(text=directory)
-        duplicatesDirectory = os.path.join(directory, DUPLICATES_DIRECTORY_NAME)
+        duplicates_directory = os.path.join(directory, DUPLICATES_DIRECTORY_NAME)
+        similarities_directory = os.path.join(directory, SIMILARITIES_DIRECTORY_NAME)
 
 def hash_file(filename):
     with open(filename , "rb") as file:
@@ -25,32 +32,55 @@ def hash_file(filename):
 
 def get_images_in_directory(directory):
     image_extensions = ('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff', '.webp')
-    images = [file for file in os.listdir(directory) if file.lower().endswith(image_extensions)]
+    images = [os.path.join(directory, file) for file in os.listdir(directory) if file.lower().endswith(image_extensions)]
     
     return images
 
-def remove_duplicate_images():
+def move_duplicate_images():
     if not directory:
         return
-    
+
     image_hashes = {}
 
     images = get_images_in_directory(directory)
 
-    if not os.path.exists(duplicatesDirectory):
-        os.makedirs(duplicatesDirectory)
+    if not os.path.exists(duplicates_directory):
+        os.makedirs(duplicates_directory)
 
     for img in images:
-        path = os.path.join(directory, img)
-        hash = hash_file(path)
+        hash = hash_file(img)
         if hash in image_hashes:
-            newPath = os.path.join(duplicatesDirectory, img)
-            os.rename(path, newPath)
+            filename = os.path.basename(img)
+            newPath = os.path.join(duplicates_directory, filename)
+            os.rename(img, newPath)
         else:
-            image_hashes[hash] = path
+            image_hashes[hash] = img
     
-    if len(os.listdir(duplicatesDirectory)) == 0:
-        os.rmdir(duplicatesDirectory)
+    if len(os.listdir(duplicates_directory)) == 0:
+        os.rmdir(duplicates_directory)
+    
+    move_similar_images()
+
+def move_similar_images():
+    if not directory:
+        return
+    
+    image_names = get_images_in_directory(directory)
+    model = SentenceTransformer('clip-ViT-B-32')
+
+    encoded_image = model.encode([Image.open(filepath) for filepath in image_names], batch_size=128, convert_to_tensor=True, show_progress_bar=True)
+
+    # Now we run the clustering algorithm. This function compares images aganist 
+    # all other images and returns a list with the pairs that have the highest 
+    # cosine similarity score
+    processed_images = util.paraphrase_mining_embeddings(encoded_image)
+
+    #duplicates = [image for image in processed_images if image[0] >= 0.999]
+    for score, image_id1, image_id2 in processed_images:
+        print("\nScore: {:.3f}%".format(score * 100))
+        print(image_names[image_id1])
+        print(image_names[image_id2])
+
 
 
 root = Tk()
@@ -64,6 +94,6 @@ ttk.Button(frm, text="->", command=select_directory).grid(column=1, row=0)
 selected_dir_label = ttk.Label(frm, text="")
 selected_dir_label.grid(column=0, row=1, columnspan=2, sticky="nsew")
 
-ttk.Button(frm, text='Proceed', command=remove_duplicate_images).grid(column=0, row=2, columnspan=2, sticky='nsew')
+ttk.Button(frm, text='Proceed', command=move_duplicate_images).grid(column=0, row=2, columnspan=2, sticky='nsew')
 
 root.mainloop()
